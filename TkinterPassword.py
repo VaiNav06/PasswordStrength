334b4ba49e81578359ecbace867b29f9717feca9
Vaijayanthimala Navneet <vaijunavneet2019@gmail.com>
2022-02-05 11:33:49 +0530

    Added a new file

---
+++ TkinterPassword.py
@@ -0,0 +1,55 @@
+import re
+import tkinter as tk
+
+def DisplayStrength(canvas):
+    password = textField.get()
+
+    def testPasswordStrength(password):
+        
+
+            eightCharsLongRegex = re.compile(r'[\w\d\s\W\D\S]{8,}')
+            upperCaseRegex = re.compile(r'[A-Z]+')
+            lowerCaseRegex = re.compile(r'[a-z]+')
+            oneOrMoreDigitRegex = re.compile(r'\d+')
+            SpecialcharecterRegex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+            if not SpecialcharecterRegex.search(password):
+                return False
+            if not eightCharsLongRegex.search(password):
+                return False
+            elif not upperCaseRegex.search(password):
+                return False
+            elif not lowerCaseRegex.search(password):
+                return False
+            elif not oneOrMoreDigitRegex.search(password):
+                return False
+
+            return True
+    info = testPasswordStrength(password)
+    if info == 0:
+        label1.config(text = "False")
+    if info == 1:
+        label1.config(text = "True")
+
+canvas = tk.Tk()
+canvas.geometry("200x300")
+canvas.title("Password App")
+canvas.configure(bg = 'pink')
+
+f = ("TimesNewRoman", 12, "bold")
+
+textField = tk.Entry(canvas, font = f)
+textField.pack(pady = 20)
+textField.focus()
+textField.bind('<Return>', DisplayStrength)
+
+label1 = tk.Label(canvas, font = f)
+label1.pack()
+label1.configure(bg = "white")
+
+label2 = tk.Label(canvas, font = f)
+label2.config(text = "If strong password, program returns True" + "\n" + "If not, program returns False")
+label2.pack()
+label2.configure(bg = "white")
+
+
+canvas.mainloop()   
